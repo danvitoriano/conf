8911b7a249e905cd4574024341360013d6e53864
Daniel Galvão <daniel.galvao@justdigital.com.br>
2017-06-16 15:58:59 -0300

    Add buy tickets button and midia kit link

--- src/components/Infos.js
+++ src/components/Infos.js
@@ -7,7 +7,7 @@ import Item from './Item';
 
 const styles = {
   container: css({
-    
+
     alignSelf: 'flex-end',
     display: 'flex',
     '@media(max-width: 720px)': {
@@ -17,12 +17,39 @@ const styles = {
       margin:'0 auto'
     }
   }),
+  link: css({
+    color: '#666666',
+    padding: '10px 15px',
+    background: Globals.colors.primary,
+    borderRadius: '5px',
+    cursor: 'pointer',
+    border: '1px solid white',
+    textDecoration: 'none',
+
+    ':hover' : {
+      backgroundColor: 'transparent',
+      color: Globals.colors.primary,
+    },
+
+    '@media(max-width: 720px)': {
+      display: 'block',
+      textAlign: 'center'
+    },
+  }),
 };
 
 class Infos extends PureComponent {
   render() {
     return (
       <div {...styles.container}>
+        <Item title="INGRESSO" subtitle="1º LOTE">
+          <p>
+            <a href=""  {...styles.link}>
+              <b>INSCREVA-SE</b>
+            </a>
+          </p>
+        </Item>
+
         <Item title="DATA" subtitle={Globals.date.full}>
           <p>{Globals.date.hour}</p>
         </Item>
