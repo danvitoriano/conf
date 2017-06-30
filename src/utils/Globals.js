1ae3ef2ce2f34d3e3a6acd9fac7163162b992802
Daniel Galvão <galvaodev@gmail.com>
2017-06-30 15:00:25 -0300

    Fix texts

--- src/utils/Globals.js
+++ src/utils/Globals.js
@@ -28,15 +28,11 @@ const Globals = {
 
   pitch: {
     title: 'COMUNIDADE REACT BRASIL',
-    text: 'A primeira React Conf da América Latina é no Brasil. Inspirados nas conferências dos Estados Unidos e Europa, queremos reunir em um único dia o bleeding edge dos desenvolvedores e empresas de tecnologia, com um único propósito: move foward.'
+    text: 'A primeira React Conf da América Latina é no Brasil. Inspirados nas conferências dos Estados Unidos e Europa, queremos reunir em um único dia o bleeding edge dos desenvolvedores e empresas de tecnologia, com um único propósito: move forward.'
   },
   topics: {
     title: 'O QUE QUEREMOS VER',
     text: 'Votação no GitHub propondo assuntos:'
-  },
-  tickets: {
-    title: '1 LOTE',
-    price: 'A consultar'
   }
 };
 
