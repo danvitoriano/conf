f87c96542e1b1926a57aec93ff69d70b693be0e2
Danilo Vitoriano <vitoriano08@gmail.com>
2017-08-18 02:52:59 -0300

    remove unused topics

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -32,36 +32,14 @@ class Topics extends PureComponent {
           </p>
           <div {...styles.container}>
             <Card topic="REACT NATIVE" author="sibelius" votes="28+" />
-            <Card topic="REACT + REDUX" author="Kirmayrtomaz" votes="25+" />
+            <Card topic="REDUX" author="Kirmayrtomaz" votes="25+" />
             <Card topic="CASES DE SUCESSO" author="phelippepuhle" votes="17+" />
             <Card topic="FIBER" author="sibelius" votes="15+" />
             <Card topic="SERVER SIDE RENDERING" author="dleitee" votes="13+" />
             <Card topic="RELAY" author="sibelius" votes="13+" />
             <Card topic="GraphQL + Apollo" author="sebas5384" votes="9+" />
-            <Card
-              topic="REACT NATIVE + FIREBASE"
-              author="rodrigooler"
-              votes="8+"
-            />
-            <Card
-              topic="BEST PRATICES FOR REUSABLE UI COMPONENTS"
-              author="rodrigooler"
-              votes="11+"
-            />
-            <Card topic="TDD + REACT" author="she-dev" votes="10+" />
-            <Card topic="PWA + REDUX" author="danvitoriano" votes="10+" />
-            <Card topic="PWA + REACT" author="fdaciuk" votes="12+" />
-            <Card
-              topic="REACT NATIVE OFFLINE FIRST"
-              author="soutot"
-              votes="8+"
-            />
-            <Card topic="RELAY SUBSCRIPTIONS" author="sibelius" votes="9+" />
-            <Card
-              topic="PUSH NOTIFICATION + REACT NATIVE"
-              author="felipedaraujo"
-              votes="9+"
-            />
+            <Card topic="TDD" author="she-dev" votes="10+" />
+            <Card topic="PWA REACT" author="fdaciuk" votes="12+" />
           </div>
         </Text>
       </div>
